629a2d001b1811263e0946b1c4730605c29329c0
Denny0097 <qctmo729@gmail.com>
2025-03-04 14:25:50 +0800

    Implement queue remove function
    through list entry function to get position of next or previous element,
    then remove it.
    Change-Id: Icf12793f20b53986d43b847156ec81f9a9e128a5

--- queue.c
+++ queue.c
@@ -66,14 +66,33 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *pos = list_entry(head->next, element_t, list);
 
-    return NULL;
+    if (sp && bufsize) {
+        strncpy(sp, pos->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    list_del(&pos->list);
+    return pos;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *pos = list_entry(head->prev, element_t, list);
+
+    if (sp && bufsize) {
+        strncpy(sp, pos->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    list_del(&pos->list);
+    return pos;
 }
 
 /* Return number of elements in queue */
